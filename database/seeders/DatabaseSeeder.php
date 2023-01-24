5da42b6aec8d28b7b4e2363086075aa6baf66772
Bradley Evans <bradleyt.evans@gmail.com>
2023-01-24 12:55:01 -0500

    created Factory's for Category and Product, and made DatabaseSeeder call on all three of my factories to generate fake data for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -18,47 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Product::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $furniture = Category::create([
-            'name' => 'Furniture',
-            'slug' => 'furniture'
+        $user =    User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $misc = Category::create([
-            'name' => 'Misc',
-            'slug' => 'misc'
+        Product::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $pet = Category::create([
-            'name' => 'Pet',
-            'slug' => 'pet'
-        ]);
-
-        Product::create([
-            'user_id' => $user->id,
-            'category_id' => $furniture->id,
-            'title' => 'Lorem Ipsum',
-            'slug' => 'lorem-ipsum',
-            'excerpt' => 'Lorem Ipsum dolor',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus optio voluptatem minus. Fuga animi delectus dolorem debitis, sed officiis quia tempora vel accusantium laboriosam non voluptatum, nam excepturi exercitationem? Laboriosam!'
-        ]);
-
-        Product::create([
-            'user_id' => $user->id,
-            'category_id' => $misc->id,
-            'title' => 'Lorem Ipsum',
-            'slug' => 'lorem-ipsum',
-            'excerpt' => 'Lorem Ipsum dolor',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus optio voluptatem minus. Fuga animi delectus dolorem debitis, sed officiis quia tempora vel accusantium laboriosam non voluptatum, nam excepturi exercitationem? Laboriosam!'
-        ]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
