1562962bd6de07f8ee066ed12f8dae90d9275a53
z0mbiebrad <87036647+z0mbiebrad@users.noreply.github.com>
2023-02-03 09:17:52 -0500

    Added some styling to the category drop down

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -5,7 +5,7 @@
         {{ $trigger }}
    </div>
     {{-- Links --}}
-    <div x-show="show" class="absolute z-50 mt-2 w-full rounded-xl bg-gray-100 py-2 overflow-auto max-h-52" style="display: none">
+    <div x-show="show" class="absolute z-50 mt-2 w-full rounded-xl bg-black bg-opacity-50 py-2 overflow-auto max-h-28" style="display: none">
         {{ $slot }}
     </div>
 
--- resources/views/components/gallery-header.blade.php
+++ resources/views/components/gallery-header.blade.php
@@ -1,13 +1,14 @@
-<header class="mx-auto mt-20 max-w-xl text-center">
+<header class="mx-auto mt-20 max-w text-center">
     <h1 class="text-6xl my-12 text-violet-50"><a href="/gallery">Gallery</a></h1>
 
-    <div class="mt-4 space-y-2 lg:space-y-0 lg:space-x-4">
+    <div class="mt-4 space-y-2 lg:space-y-0 lg:space-x-4 mb-20">
         <!--  Category -->
-        <div class="relative rounded-xl bg-gray-100 lg:inline-flex">
+        <div
+            class="bg-black bg-opacity-50 text-sm font-semibold text-center text-violet-50 shadow-black shadow-md rounded-2xl relative  lg:inline-flex">
             <x-dropdown>
                 <x-slot name="trigger">
                     <button
-                        class="inline-flex w-full flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-left text-sm font-semibold lg:w-32">
+                        class="inline-flex w-full flex-1 appearance-none bg-transparent py-2 px-16 text-sm font-semibold text-center mx-auto justify-items-center">
 
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
@@ -17,10 +18,8 @@
                     All
                 </x-dropdown-item>
                 @foreach ($categories as $category)
-                    <x-dropdown-item
-                        href="/category/{{ $category->slug }}"
-                        :active="isset($currentCategory) && $currentCategory->is($category)"
-                        >{{ ucwords($category->name) }}
+                    <x-dropdown-item href="/category/{{ $category->slug }}" :active="isset($currentCategory) && $currentCategory->is($category)">
+                        {{ ucwords($category->name) }}
                     </x-dropdown-item>
                 @endforeach
             </x-dropdown>
@@ -28,13 +27,13 @@
 
 
 
-    <div
-        class="relative flex items-center lg:inline-flex bg-black bg-opacity-70 shadow-black shadow-md rounded-2xl text-violet-50">
-        <form method="GET" action="#">
-            <input type="text" name="search" placeholder="Find something..."
-                class="bg-black bg-opacity-50 text-sm font-semibold placeholder-black focus:ring-transparent border-none !outline-none focus:border-none text-center placeholder:text-violet-50 shadow-black shadow-md rounded-2xl"
-                value="{{ request('search') }}">
-        </form>
-    </div>
+        <div
+            class="relative flex items-center lg:inline-flex rounded-2xl text-violet-50 px-2">
+            <form method="GET" action="#">
+                <input type="text" name="search" placeholder="Find something..."
+                    class="bg-black bg-opacity-50 text-sm font-semibold placeholder-black focus:ring-transparent border-none !outline-none focus:border-none text-center placeholder:text-violet-50 shadow-black shadow-md rounded-2xl"
+                    value="{{ request('search') }}">
+            </form>
+        </div>
     </div>
 </header>
