8a0b96c4172d424313f13c481eb8bd0064332fde
z0mbiebrad <87036647+z0mbiebrad@users.noreply.github.com>
2023-02-21 15:19:56 -0500

    Updated views to proper naming convention
    Got rid of some old files
    
    Moved views into new home folder for the proper naming conventions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,13 +10,13 @@ class PostController extends Controller
 
     public function index()
     {
-        return view('home', [
+        return view('home.index', [
             'categories' => Category::all()
         ]);
     }
     public function gallery()
     {
-        return view('gallery', [
+        return view('home.gallery', [
             'products' => Product::latest()->filter(request(['search', 'category']))->get(),
         ]);
     }
--- resources/views/miscellaneous.blade.php
+++
@@ -1,1 +0,0 @@
-<x-products></x-products>
