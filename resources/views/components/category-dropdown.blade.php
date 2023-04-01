0c7c90fc4b64e488ae8b56f98cb91f597f4c02d6
z0mbiebrad <87036647+z0mbiebrad@users.noreply.github.com>
2023-04-01 19:35:15 -0400

    Query build
    Added 'page' to the query builder except list so the query scope works properly.
    Added the pagination pages incase I wanted to edit design.

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,12 +7,12 @@
 
         </button>
     </x-slot>
-    <x-dropdown-item href="/gallery/?{{ http_build_query(request()->except('category')) }}" :active="request()->routeIs('home')">
+    <x-dropdown-item href="/gallery/?{{ http_build_query(request()->except('category', 'page')) }}" :active="request()->routeIs('home')">
         All
     </x-dropdown-item>
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/gallery/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            href="/gallery/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             active:="request()->is('categories/{$category->slug}')">
             {{ ucwords($category->name) }}
         </x-dropdown-item>
