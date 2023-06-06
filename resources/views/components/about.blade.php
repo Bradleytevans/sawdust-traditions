42ba7a25f6f20573e09464f69980284e884d8262
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-06-05 21:03:51 -0400

    finish phone styling for index page

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,10 +1,9 @@
-<div class="bg-black bg-opacity-70 flex text-gray-200 pt-2 pb-14 shadow-black shadow-md">
-    <div class=" w-1/2 ml-20 py-14 px-20">
-        <h1
-            class="align-middle shadow-md shadow-black bg-opacity-60 text-semibold text-3xl my-10 py-7 text-gray-900 bg-white m-auto rounded-3xl">
+<div class="bg-black bg-opacity-70 flex text-gray-200 shadow-black shadow-md grid grid-cols-1">
+    <div class="w-5/6 py-2 px-2 mx-auto">
+        <h1 class="align-middle shadow-md shadow-black bg-opacity-60 text-semibold text-2xl my-10 py-7 text-gray-900 bg-white m-auto rounded-3xl">
             Experience & History
         </h1>
-        <p class="shadow-black shadow-md bg-black bg-opacity-30 rounded-3xl p-3">
+        <p class="shadow-black shadow-md bg-black bg-opacity-30 rounded-3xl p-3 text-lg">
             Phillip Evans has more than 40+ years of experience in construction, carpentry, and welding. His father,
             William (Bill) Evans, was the owner of a construction company that specialized in building houses, churches,
             and businesses. Having tagged along on most of Bill's jobs, he learned the trade and has passed on these
@@ -12,34 +11,26 @@
         </p>
 
         <div class="flex justify-between">
-            <img src="{{ asset('images/evans2.png') }}" alt=""
-                class="rounded-lg mt-10 mr-2 shadow-black shadow-lg w-1/2">
-            <img src="{{ asset('images/evans1.png') }}" alt=""
-                class="rounded-lg mt-10 ml-2 shadow-black shadow-lg w-1/2">
+            <img src="{{ asset('images/evans2.png') }}" alt="" class="rounded-lg mt-10 mr-2 shadow-black shadow-lg w-1/2">
+            <img src="{{ asset('images/evans1.png') }}" alt="" class="rounded-lg mt-10 ml-2 shadow-black shadow-lg w-1/2">
         </div>
     </div>
-    <div class=" w-1/2 mr-20 py-14 px-20 ">
-        <h1
-            class="align-middle shadow-md shadow-black bg-opacity-60 text-semibold text-3xl my-10 py-7 text-gray-900 bg-white m-auto rounded-3xl">
+    <div class="w-5/6 py-2 px-2 mx-auto">
+        <h1 class="align-middle shadow-md shadow-black bg-opacity-60 text-semibold text-2xl my-10 py-7 text-gray-900 bg-white m-auto rounded-3xl">
             Our Process
         </h1>
-        <p class="shadow-black shadow-md bg-black bg-opacity-30 rounded-3xl p-3">
+        <p class="shadow-black shadow-md bg-black bg-opacity-30 rounded-3xl p-3 text-lg">
             Each piece is created with the customer at heart. Whether it is a table, bed frame, bookshelf, or elevated
             dog bowls, Sawdust Traditions aims to please. Individual and personalized consultations are held over the
             phone, via email, or in-person to understand the customer's vision and bring it to life. If you are
             interested in a consultation, please submit the custom quote form and we will contact you to discuss your
             idea.
         </p>
-        <div class="flex justify-between">
-            <div class="">
-                <img src="{{ asset('images/phill2.png') }}" alt=""
-                    class="rounded-lg mt-10 shadow-black shadow-lg h-2/5">
-                <img src="{{ asset('images/phill1.png') }}" alt=""
-                    class="rounded-lg mt-9 shadow-black shadow-lg h-2/5">
+        <div class="flex justify-between mb-8">
+            <div>
+                <img src="{{ asset('images/phill2.png') }}" alt="" class="rounded-lg mt-10 shadow-black shadow-lg h-2/5">
+                <img src="{{ asset('images/phill1.png') }}" alt="" class="rounded-lg mt-9 shadow-black shadow-lg h-2/5">
             </div>
-
-            <img src="{{ asset('images/phillip4.jpg') }}" alt=""
-                class="rounded-lg mt-10 shadow-black shadow-lg w-1/2 h-1/3">
         </div>
     </div>
 </div>
