4d7b848971b9159b3ff18a1377902d440409fea3
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-06-15 15:31:22 -0400

    finished styling for all styles and some picture swaps.

--- resources/views/components/products.blade.php
+++ resources/views/components/products.blade.php
@@ -1,8 +1,8 @@
-<div class="bg-opacity-0">
-    <div class="max-w-2xl p-2 mx-auto mb-16 lg:max-w-7xl lg:px-8">
+<div class="mx-auto bg-opacity-0">
+    <div class="p-2 mx-auto mb-16 ">
         <h3 class="w-5/6 p-6 mx-auto mt-10 mb-12 text-2xl text-center bg-gray-200 shadow-md shadow-black bg-opacity-70 rounded-3xl">
             Products</h3>
-        <div class="grid w-5/6 grid-cols-1 gap-4 mx-auto sm:grid-cols-2 xl:grid-cols-4 xl:gap-x-12">
+        <div class="grid w-5/6 grid-cols-1 gap-10 mx-auto lg:gap-20 xl:gap-32 sm:grid-cols-2">
             @foreach ($homeProducts as $product)
             <a href="/gallery/?category={{ $product->slug }}" class="">
                 <div class="mx-auto overflow-hidden bg-gray-200 shadow-md hover:bg-opacity-70 hover:text-gray-200 hover:bg-gray-800 shadow-black rounded-3xl bg-opacity-80">
--- resources/views/components/whatwedo.blade.php
+++ resources/views/components/whatwedo.blade.php
@@ -1,12 +1,12 @@
-<div class="bg-black text-white bg-opacity-70 md:flex shadow-black shadow-md">
-    <div class="flex-1 p-2 xl:mx-2 w-5/6 mx-auto ">
-        <h4 class="align-middle shadow-md shadow-black bg-opacity-60 text-semibold text-2xl my-10 py-7 text-gray-900 bg-white m-auto rounded-3xl">
+<div class="text-white bg-black shadow-md bg-opacity-70 shadow-black">
+    <div class="flex-1 w-5/6 p-2 mx-auto">
+        <h4 class="m-auto my-10 text-2xl text-gray-900 align-middle bg-white shadow-md shadow-black bg-opacity-60 text-semibold py-7 rounded-3xl">
             What We
             Do</h4>
-        <p class="shadow-black shadow-md rounded-3xl bg-opacity-30 bg-black mt-7 mb-15 text-gray-200 text-lg p-3 mb-10">
+        <p class="p-3 mb-10 text-lg text-gray-200 bg-black shadow-md shadow-black rounded-3xl bg-opacity-30 mt-7 mb-15">
             Sawdust Traditions is a family-owned woodworking company founded by Phillip and Jennifer Evans. They are committed to producing high-quality, durable wood products at affordable prices. Every item is meticulously handcrafted, tailored to the client's vision, and combines artistic beauty with functional furniture.
             Have an idea? <a href="/" class="underline hover:text-gray-900">Contact</a> us and we will bring it to life!
         </p>
-        <img src="{{ asset('images/drill.jpg') }}" class="rounded-3xl shadow-lg shadow-black mb-10 mx-auto">
+        <img src="{{ asset('images/showcase.jpg') }}" class="mx-auto mb-10 shadow-lg rounded-3xl shadow-black h-1/5">
     </div>
 </div>
